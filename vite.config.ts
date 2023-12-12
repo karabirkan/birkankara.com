c3a6089e89fd9e7e4359d9b1c6054ae79a534e52
Birkan Kara <115654236+karabirkan@users.noreply.github.com>
2023-12-12 13:13:06 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/birkankara.com/"
+  base:"/birkankara.com/",
   plugins: [react()],
 });
